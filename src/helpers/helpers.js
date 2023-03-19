2e39e726a808ed877ff78acea95d35ec5f1d2155
Paulo Torrealba <paulo@buscalibre.com>
2023-03-19 03:23:38 -0300

    con iconos temporales

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -6,53 +6,6 @@ export function elconsole(elparam) {
 	// console.debug( "CLICK" );
 }
 
-// export async function ladefinicion( palabrabuscada ) {
-// 	console.debug( `lo buscado: ${palabrabuscada}` );
-// 	const url = `https://es.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=1&titles=${palabrabuscada}&explaintext=1&exsectionformat=plain&format=json&lang=es`;
-// 	// const res = await fetch(url);
-// 	// const data = await res.json();
-// 	console.debug( url );
-// 	// return palabrabuscada;
-// }
-function traigo(buscar) {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			// const url = `https://es.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=1&titles=${buscar}&explaintext=1&exsectionformat=plain&format=json&lang=es`;
-			const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${buscar}`;
-			fetch(url).then((x) => x.json()).then((x) => {
-				resolve(x);
-			});
-		}, 2000);
-	});
-}
-
-async function leimprimi(buscanding) {
-	console.debug( "llamanding" );
-	const resultado = await traigo(buscanding);
-	// console.debug( `Definicion? ${resultado}` );
-	console.debug( resultado );
-	// console.debug( resultado.meanings.definitions.definition );
-	console.debug( resultado[0].meanings[0].definitions[0].definition );
-	console.debug( "cortanding" );
-	const laadefinicion = resultado[0].meanings[0].definitions[0].definition;
-	return laadefinicion;
-	// return resultado;
-}
-
-export async function getServerSideProps() {
-	const res = await fetch("https://es.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=1&titles=pizza&explaintext=1&exsectionformat=plain&format=json&lang=es");
-	const data = await res.json();
-
-	const testdefinicion = data.query.pages[Object.keys(data.query.pages)].extract;
-
-	return {
-		props: {
-			todos: data,
-			testdefinicion,
-		},
-	};
-}
-
 export const callAPI = async function (buscado) {
 	try {
 		const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${buscado}`);
@@ -63,19 +16,5 @@ export const callAPI = async function (buscado) {
 		console.debug("hubo un error !");
 		console.debug(err);
 		return err;
-	}
-};
-
-export const callAPIreloaded = async function () {
-	try {
-		const res = await fetch(
-			"https://api.dictionaryapi.dev/api/v2/entries/en/pizza",
-		);
-		const data = await res.json();
-		const frase = data[0].meanings[0].definitions[0].definition;
-		console.debug(frase);
-		return frase;
-	} catch (err) {
-		console.debug(err);
 	}
 };
