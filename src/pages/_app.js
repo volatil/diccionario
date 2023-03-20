c3e3aaf84e136c8673fef424e4e18b6dd455ec61
Paulo Torrealba <paulo@buscalibre.com>
2023-03-20 02:10:32 -0300

    fix to deploy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/home.css";
 
 export default function App({ Component, pageProps }) {
+	/* eslint-disable */
 	return <Component {...pageProps} />;
+	/* eslint-enable */
 }
